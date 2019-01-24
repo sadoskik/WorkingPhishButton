4118abadd7ecbcbeb717ac5374e131dd09063e24
David Sadoski <sadoskik@gmail.com>
2019-01-24 17:16:11 -0500

    Removed old forwardasattachemntt

--- OutlookEmailForwarderWeb/App/Home/OutlookEmailForwarder.js
+++ OutlookEmailForwarderWeb/App/Home/OutlookEmailForwarder.js
@@ -116,9 +116,6 @@
                     '        <t:ForwardItem>' +
                     '          <t:Subject>' + '[Phishing][From: ' + mailbox.userProfile.emailAddress + ']' + '</t:Subject>' +
                     '          <t:ToRecipients>' + "<t:Mailbox><t:EmailAddress>" + 'sadoskik@gmail.com' + "</t:EmailAddress></t:Mailbox>" + '</t:ToRecipients>' +
-                    '           <ForwardAsAttachmentToRecipients>'+
-                    ' < Address ><t:EmailAddress> sadoskik@gmail.com </t:EmailAddress></Address>' 
-                    ' </ForwardAsAttachmentToRecipients >'      +
                     '          <t:ReferenceItemId Id="' + item_id + '" ChangeKey="' + changeKey + '" />' +
                     '          <t:NewBodyContent BodyType="Text">' + comment + '</t:NewBodyContent>' +
                     '        </t:ForwardItem>' +
