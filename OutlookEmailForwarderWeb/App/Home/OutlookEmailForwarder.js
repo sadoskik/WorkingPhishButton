794874add71df712dd34a39a500436f41831f8ab
David Sadoski <sadoskik@gmail.com>
2019-01-24 22:57:39 -0500

    zxx

--- OutlookEmailForwarderWeb/App/Home/OutlookEmailForwarder.js
+++ OutlookEmailForwarderWeb/App/Home/OutlookEmailForwarder.js
@@ -162,7 +162,7 @@
                 if (result == "NoError") {
                     try {
 
-                        app.showNotification("Thank you for taking this action. We will respond with our analysis soon.2" + xmlDoc.toString(), 'Georgia Tech SOC');
+                        app.showNotification("Thank you for taking this action. We will respond with our analysis soon.3" + xmlDoc.toString(), 'Georgia Tech SOC');
                     }
                     catch (e) {
                         app.showNotification('This is the error', e);
